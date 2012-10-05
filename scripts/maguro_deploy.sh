306fea42b2f7aff53fc30107ec88e7ac365cd90d
Loïc Molinari <loic.molinari@canonical.com>
2012-10-05 19:24:10 +0200

    Renamed the MultiTouch.qml example to inpuInput.qml.

--- scripts/maguro_deploy.sh
+++ scripts/maguro_deploy.sh
@@ -5,6 +5,6 @@ adb push ../plugins/platforms/libqhybris.so /data/ubuntu/home/ufa/qt5/platforms
 adb push ../examples/hybris-qmlscene /data/ubuntu/home/ufa/qt5/examples
 adb push ../examples/MovingLogo.qml /data/ubuntu/home/ufa/qt5/examples
 adb push ../examples/WarpingLogo.qml /data/ubuntu/home/ufa/qt5/examples
-adb push ../examples/MultiTouch.qml /data/ubuntu/home/ufa/qt5/examples
+adb push ../examples/Input.qml /data/ubuntu/home/ufa/qt5/examples
 adb push ../examples/logo.png /data/ubuntu/home/ufa/qt5/examples
 adb push ../examples/noise.png /data/ubuntu/home/ufa/qt5/examples
