112450bceddf04ac63af4a1d08d3ca0afb23984b
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2012-12-04 09:16:51 -0300

    Get the icon theme name from QHYBRIS_ICON_THEME enviroment variable.

--- src/platforms/base/theme.cc
+++ src/platforms/base/theme.cc
@@ -12,9 +12,15 @@ QHybrisTheme::QHybrisTheme() {
 
 QVariant QHybrisTheme::themeHint(ThemeHint hint) const {
   DLOG("QHybrisTheme::themehint (this=%p, hint=%d)", this, hint);
-  if (hint == QPlatformTheme::SystemIconThemeName)
-    return QVariant(QString(QStringLiteral("ubuntu-mono-dark")));
-  else
+  if (hint == QPlatformTheme::SystemIconThemeName) {
+    QByteArray iconTheme = qgetenv("QHYBRIS_ICON_THEME");
+    if (iconTheme.isEmpty()) {
+      return QVariant(QStringLiteral("ubuntu-mobile"));
+    } else {
+      return QVariant(QString(iconTheme));
+    }
+  } else {
     return QGenericUnixTheme::themeHint(hint);
+  }
 }
 
