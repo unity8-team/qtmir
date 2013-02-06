03fbd8be692124f5c4036ce20c912681a41ff74c
Loïc Molinari <loic.molinari@canonical.com>
2013-02-06 02:47:53 +0100

    Fixed deploy.sh module installation paths.

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 
 adb push src/platforms/ubuntu/libqubuntu.so /data/ubuntu/usr/lib/arm-linux-gnueabihf/qt5/plugins/platforms
 adb push src/platforms/ubuntulegacy/libqubuntulegacy.so /data/ubuntu/usr/lib/arm-linux-gnueabihf/qt5/plugins/platforms
-# FIXME(loicm) Not sure here to install that for now?
-#adb push src/modules/application/libubuntuapplicationplugin.so /data/ubuntu/usr/???/Ubuntu/Application
-#adb push src/modules/application/qmldir /data/ubuntu/usr/???/Ubuntu/Application
+adb push src/modules/application/libubuntuapplicationplugin.so /data/ubuntu/usr/lib/arm-linux-gnueabihf/qt5/imports/Ubuntu/Application
+adb push src/modules/application/qmldir /data/ubuntu/usr/lib/arm-linux-gnueabihf/qt5/imports/Ubuntu/Application
 adb push examples/qmlscene-ubuntu /data/ubuntu/usr/bin
 adb push examples/Logo.qml /data/ubuntu/usr/lib/arm-linux-gnueabihf/qt5/examples
 adb push examples/MovingLogo.qml /data/ubuntu/usr/lib/arm-linux-gnueabihf/qt5/examples
