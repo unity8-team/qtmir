ba5307ecbfab08c24e309a9ef9a0fa568793f20f
Ricardo Mendoza <ricardo.mendoza@canonical.com>
2013-06-05 16:24:18 -0430

    Pass remote Pid when starting session in order to support cross-container operation

--- src/platforms/ubuntu/integration.cc
+++ src/platforms/ubuntu/integration.cc
@@ -134,6 +134,11 @@ QPlatformWindow* QUbuntuIntegration::createPlatformWindow(QWindow* window) {
     props_ = ua_ui_session_properties_new();
     ua_ui_session_properties_set_type(props_, static_cast<UAUiSessionType>(sessionType));
 
+    ua_ui_session_properties_set_remote_pid(
+        props_,
+        static_cast<uint32_t>(QCoreApplication::applicationPid())
+        );
+
     session_ = ua_ui_session_new_with_properties(props_);
 
     input_->setSessionType(sessionType);
