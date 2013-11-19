89d23af5713721b2f987b8261e4ff60ef24bbc4f
Albert Astals <albert.astals@canonical.com>
2013-11-19 18:17:37 +0100

    Don't include the QtQml catch-all header
    Include only the ones we need

--- src/modules/application/plugin.cc
+++ src/modules/application/plugin.cc
@@ -13,7 +13,9 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <QtQml/QtQml>
+#include <qqmlcomponent.h>
+#include <qqmlextensionplugin.h>
+
 #include <QtQuick/QQuickWindow>
 #include "application.h"
 #include "application_manager.h"
