fa61c860bf76a60a407cb7594c9d58d1c3048235
Robert Carr <robert.carr@canonical.com>
2014-07-30 16:15:10 -0700

    Add acceptance skeleton

---
+++ tests/qpa/acceptance-tests/acceptance-tests.pro
@@ -0,0 +1,15 @@
+include(../../test-includes.pri)
+include(../../common/common.pri)
+
+TARGET = qpa_acceptance_tests
+
+INCLUDEPATH += \
+    ../../../src/platforms/mirserver
+
+SOURCES += \
+    acceptance_tests.cpp
+
+# need to link in the QPA plugin too for access to MirServerConfiguration
+LIBS += -Wl,-rpath,$${PWD}/../../../src/platforms/mirserver \
+    -L../../../src/platforms/mirserver -lqpa-mirserver
+
---
+++ tests/qpa/acceptance-tests/acceptance_tests.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2014 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+ * SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <Unity/Application/application_manager.h>
+
+#include <Unity/Application/applicationcontroller.h>
+#include <Unity/Application/taskcontroller.h>
+#include <Unity/Application/proc_info.h>
+#include <mirserverconfiguration.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+TEST(QPAAcceptance, client_may_connect_and_exit)
+{
+    using namespace testing;
+}
