a4440a3e882966872464351b247ba3a1f1f9d73f
Robert Carr <robert.carr@canonical.com>
2014-12-15 16:51:28 -0800

    Some progress I suppose

--- src/ubuntumirclient/integration.cpp
+++ src/ubuntumirclient/integration.cpp
@@ -156,7 +156,7 @@ QPlatformWindow* UbuntuClientIntegration::createPlatformWindow(QWindow* window) 
 QPlatformWindow* UbuntuClientIntegration::createPlatformWindow(QWindow* window)
 {
     QPlatformWindow* platformWindow = new UbuntuWindow(
-            window, mClipboard, static_cast<UbuntuScreen*>(mScreen), mInput, mInstance);
+            window, mClipboard, static_cast<UbuntuScreen*>(mScreen), mInput, u_application_instance_get_mir_connection(mInstance));
     platformWindow->requestActivateWindow();
     return platformWindow;
 }
