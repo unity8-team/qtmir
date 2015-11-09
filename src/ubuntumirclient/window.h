4964f45b1a7def09afde316bfdffc224edc94f8e
Nick Dedekind <nick.dedekind@canonical.com>
2015-11-09 19:26:55 +0000

    revert occlusion detection

--- src/ubuntumirclient/window.h
+++ src/ubuntumirclient/window.h
@@ -40,14 +40,12 @@ public:
     void setGeometry(const QRect&) override;
     void setWindowState(Qt::WindowState state) override;
     void setVisible(bool visible) override;
-    bool isExposed() const override;
     void setWindowTitle(const QString &title) override;
 
     // New methods.
     void* eglSurface() const;
     void handleSurfaceResize(int width, int height);
     void handleSurfaceFocusChange(bool focused);
-    void handleSurfaceExposeChange(bool visible);
     void onBuffersSwapped_threadSafe(int newBufferWidth, int newBufferHeight);
 
     UbuntuWindowPrivate* priv() { return d; }
