0e6d57fbf08f61cdce0732e22fc977180e0e83f1
Gerry Boland <gerry.boland@canonical.com>
2016-01-05 22:43:49 +0000

    Add missing file

---
+++ src/ubuntumirclient/utils.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+ * SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <QtGlobal>
+
+namespace {
+    inline int divideAndRoundUp(int numerator, qreal denominator)
+    {
+        return ceil((qreal)numerator / denominator);
+    }
+} // anonymous namespace
+
+#endif // UTILS_H
