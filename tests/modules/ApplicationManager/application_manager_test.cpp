ca08bbcc89a8d153a4469224e9d2bf66a62c0d62
Daniel d'Andrada <daniel.dandrada@canonical.com>
2016-02-01 12:25:55 -0200

    Regression test

--- tests/modules/ApplicationManager/application_manager_test.cpp
+++ tests/modules/ApplicationManager/application_manager_test.cpp
@@ -2266,3 +2266,94 @@ TEST_F(ApplicationManagerTests,CloseWhenSuspendedAfterSessionStopped)
     EXPECT_EQ(Application::Stopped, application->state());
     EXPECT_EQ(spy.count(), 1);
 }
+
+/*
+ * Context:
+ *  - we are waiting for an application to quit following a close()
+ *
+ * Event:
+ *  - TaskController requests that application to be resumed.
+ *    Most likely because the user tapped the app icon in the Dash. From the user's
+ *    perspective the application is already closed and he wants to start a new
+ *    instance of it.
+ *    But from TaskController's perspective the application is still running and
+ *    therefore should just be resumed/focused.
+ *
+ * Expected outcome:
+ *  - ApplicationManager should wait for the closing application to finally end (or
+      be killed) and then finally launch a new instance of it.
+ *
+ * Regression test for https://bugs.launchpad.net/ubuntu/+source/qtmir/+bug/1527737
+ * "Apps do not start if restarted quickly after closing"
+ */
+TEST_F(ApplicationManagerTests, resumeRequestedForClosingApplication)
+{
+    using namespace ::testing;
+
+    int argc = 0;
+    char* argv[0];
+    QCoreApplication qtApp(argc, argv); // app for deleteLater event
+
+    const QString appId("testAppId");
+    quint64 procId = 5551;
+
+    ON_CALL(desktopFileReaderFactory, createInstance(appId, _))
+        .WillByDefault(Invoke(
+            [](const QString &appId, const QFileInfo&) { return new FakeDesktopFileReader(appId); }
+        ));
+
+    EXPECT_CALL(*taskController, start(appId, _))
+        .Times(1)
+        .WillOnce(Return(true));
+
+    auto app = applicationManager.startApplication(appId, ApplicationManager::NoFlag);
+    applicationManager.onProcessStarting(appId);
+    std::shared_ptr<mir::scene::Session> session = std::make_shared<MockSession>("", procId);
+    bool authed = true;
+    applicationManager.authorizeSession(procId, authed);
+    onSessionStarting(session);
+
+    FakeMirSurface *surface = new FakeMirSurface;
+    onSessionCreatedSurface(session.get(), surface);
+    surface->drawFirstFrame();
+
+    EXPECT_EQ(Application::InternalState::Running, app->internalState());
+
+    QSignalSpy closeRequestedSpy(surface, SIGNAL(closeRequested()));
+
+    // User swipes away the application, visually closing/removing it.
+    applicationManager.stopApplication(appId);
+
+    // But asking ApplicationManager to stop the application just makes it request its
+    // surfaces to be closed
+    EXPECT_EQ(Application::InternalState::Closing, app->internalState());
+    EXPECT_EQ(1, closeRequestedSpy.count());
+
+    QSignalSpy appAddedSpy(&applicationManager, SIGNAL(applicationAdded(const QString&)));
+
+    // User then taps on the app icon
+    applicationManager.onProcessStarting(appId);
+    applicationManager.onResumeRequested(appId);
+
+    // But no application instance should have been created because of that
+    EXPECT_TRUE(applicationManager.findApplication(appId) == nullptr);
+    EXPECT_EQ(0, appAddedSpy.count());
+
+    // ApplicationManager will ask TaskController to start the application again
+    // once the closing instance is gone
+    Mock::VerifyAndClearExpectations(taskController);
+    EXPECT_CALL(*taskController, start(appId, _))
+        .Times(1)
+        .WillOnce(Return(true));
+
+    // Simulates that the application complied to the close() request and stopped itself
+    onSessionStopping(session);
+    applicationManager.onProcessStopped(appId);
+
+    // DeferredDelete is special: likes to be called out specifically or it won't come out
+    qtApp.sendPostedEvents(app, QEvent::DeferredDelete);
+    qtApp.sendPostedEvents();
+
+    EXPECT_TRUE(applicationManager.findApplication(appId) != nullptr);
+    EXPECT_EQ(1, appAddedSpy.count());
+}
